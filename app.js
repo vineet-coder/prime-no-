1c45b470ade6633b26cb177cf347998f9076391e
vineet <vineetsinghal2312@gmail.com>
2021-01-24 04:35:26 +0530

    fx

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var noInput = document.querySelector("#txt-input");
+var noInput1 = document.querySelector("#txt-input1");
+var noInput2 = document.querySelector("#txt-input2");
+
 var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
-  var nO = noInput.value;
-  if (nO === 1) {
-    outputDiv.innerText = nO + " is not a prime Number";
-  }
-  for (var i = 2; i < nO; i++) {
-    if (nO % i === 0) {
-      outputDiv.innerText = nO + " is not a prime Number";
-      break;
-    } else {
-      outputDiv.innerText = nO + " is  a prime Number";
-    }
+  var nO1 = noInput1.value;
+  var nO2 = noInput2.value;
+
+  if (nO1 === nO2) {
+    outputDiv.innerText = "no are equal";
+  } else {
+    outputDiv.innerText = "no are not equal";
   }
 }
 
