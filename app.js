2b85e852f87fee3c2474619d583bec0f72a2843f
vineet <vineetsinghal2312@gmail.com>
2021-01-26 03:15:54 +0530

    fix

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var noInput1 = document.querySelector("#txt-input1");
-var noInput2 = document.querySelector("#txt-input2");
-
 var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
   var nO1 = noInput1.value;
-  var nO2 = noInput2.value;
-
-  if (nO1 === nO2) {
-    outputDiv.innerText = "no are equal";
+  if (nO1 === 1) {
+    outputDiv.innerText = "no.is Prime ddddssaasd";
+    break;
+  } else if (nO1 > 1) {
+    for (var i = 2; i < nO1; i++) {
+      if (nO1 % i === 0) {
+        outputDiv.innerText = "no.is not Prime";
+        break;
+      } else {
+        outputDiv.innerText = "no.is Prime";
+      }
+    }
   } else {
-    outputDiv.innerText = "no are not equal";
+    outputDiv.innerText = "no.is not Prime";
   }
 }
 
