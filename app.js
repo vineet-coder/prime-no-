7548e10ec09573008b5990915f1712d40f914d0b
vineet <vineetsinghal2312@gmail.com>
2021-01-26 17:58:06 +0530

    fix

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
   var nO1 = noInput1.value;
-  if (nO1 === 1) {
+  if (nO1 == 1) {
     outputDiv.innerText = "no.is Prime ddddssaasd";
-  } else if (nO1 > 1) {
-    for (var i = 2; i < nO1; i++) {
-      if (nO1 % i === 0) {
-        outputDiv.innerText = "no.is not Prime";
-        break;
-      } else {
-        outputDiv.innerText = "no.is Prime";
-      }
-    }
   } else {
-    outputDiv.innerText = "no.is not Prime";
+    if (nO1 > 1) {
+      for (var i = 2; i < nO1; i++) {
+        if (nO1 % i === 0) {
+          outputDiv.innerText = "no.is not Prime";
+          break;
+        } else {
+          outputDiv.innerText = "no.is Prime";
+        }
+      }
+    } else {
+      outputDiv.innerText = "no.is not Prime";
+    }
   }
 }
 
